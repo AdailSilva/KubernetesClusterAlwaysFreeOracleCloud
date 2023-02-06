4ca02ee3968c1b296ac3ee9600586dfeca777256
Adail Silva <ass8@bdmg.mg.gov.br>
2023-02-05 21:40:34 -0300

    Change subnet IPs

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ locals {
     for i in range(1, 1 + var.how_many_nodes) :
     i => {
       node_name  = format("adailsilva-node-%d", i)
-      ip_address = format("10.0.0.%d", 10 + i)
+      ip_address = format("192.168.0.%d", 100 + i)
       role       = i == 1 ? "controlplane" : "worker"
     }
   }
